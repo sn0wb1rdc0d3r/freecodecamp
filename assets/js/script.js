48fd50f7efce0d4ecd465c493b32a60a7d3300e3
Nate Morton <nathan.j.morton@gmail.com>
2017-03-31 00:21:19 -0700

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -307,11 +307,6 @@ function handleChunky(){
   }
   chunkArrayInGroups(["a", "b", "c", "d"], 2);
   chunkArrayInGroups(["a", "b", "c", "d"], 2) // should return [["a", "b"], ["c", "d"]].
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) //should return [[0, 1, 2], [3, 4, 5]].
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) //should return [[0, 1], [2, 3], [4, 5]].
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) //should return [[0, 1, 2, 3], [4, 5]].
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) //should return [[0, 1, 2], [3, 4, 5], [6]].
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4) //should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2) //should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+  
 }
 $(document).ready(handleChunky);
