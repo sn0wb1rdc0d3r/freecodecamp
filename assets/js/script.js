3908591faf93f7bad880a6cecb436a3c95fe732c
Nate Morton <nathan.j.morton@gmail.com>
2017-04-02 19:48:17 -0700

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -374,13 +374,3 @@
   destroyer(["tree", "hamburger", 53], "tree", 53) //should return ["hamburger"].
 
 })();
-
-
-
-
-function isBigEnough(value) {
-  return value >= 10;
-}
-
-var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
-// filtered is [12, 130, 44]
